cb8274e023ff83c95363c8d0dbf16f21a60e77cb
Federico Rota <federico.rota01@gmail.com>
2015-09-28 22:01:58 +0200

    Integrazione ng-img-crop

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-var mymodal = angular.module('mymodal', []);
+var mymodal = angular.module('mymodal', ['ngImgCrop']);
 
 mymodal.controller('MainCtrl', function ($scope) {
     $scope.showModal = false;
     $scope.toggleModal = function(){
         $scope.showModal = !$scope.showModal;
     };
+         $scope.myImage='';
+        $scope.myCroppedImage='';
+
+        var handleFileSelect=function(evt) {
+          var file=evt.currentTarget.files[0];
+          var reader = new FileReader();
+          reader.onload = function (evt) {
+            $scope.$apply(function($scope){
+              $scope.myImage=evt.target.result;
+            });
+          };
+          reader.readAsDataURL(file);
+        };
+        angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
+
   });
 
 mymodal.directive('modal', function () {
